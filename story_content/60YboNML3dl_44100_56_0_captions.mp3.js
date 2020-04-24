cc7baeafdae79afda3fc142d3b0bc9cbda8eaf85
Brian K Langford <47423650+bklangford@users.noreply.github.com>
2020-04-24 13:24:27 -0700

    Add files via upload

---
+++ story_content/60YboNML3dl_44100_56_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.37.21453.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.244%0D%0AFSC%20Securities%20Corporation%20was%20founded%20in%201958.%20%0D%0A%0D%0A"}')
---
+++ story_content/64EYP82oQBY_44100_56_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.37.21453.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:02.993%0D%0ACulture%20is%20what%20drives%20excellence%20in%20a%20great%20company.%0D%0A%0D%0A"}')
