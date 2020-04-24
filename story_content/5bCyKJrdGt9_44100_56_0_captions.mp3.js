d9a82f400cdda40e244d29288df5587abe3c5100
Brian K Langford <47423650+bklangford@users.noreply.github.com>
2020-04-24 13:12:59 -0700

    Add files via upload

---
+++ story_content/5bCyKJrdGt9_44100_56_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.37.21453.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:01.674%0D%0AEach%20firm%20seeks%20to:%0D%0A%0D%0A"}')
---
+++ story_content/5bGTTK3BxUK_44100_56_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.37.21453.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:02.816%0D%0ASo%20how%20did%20these%20separate%20firms%20eventually%20become%20one%20company?%0D%0A%0D%0A"}')
